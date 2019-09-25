73f216f9a122359bb730c9a8e7fef6827b941b07
Guillem Orellana Trullols <guillem.orellana@gmail.com>
2019-09-25 09:16:30 +0200

    Update map-generator.h

--- include/map-generator.h
+++ include/map-generator.h
@@ -14,14 +14,20 @@ enum Action {
     NONE
 };
 
-class Successor {
+/* Class which represents the possible children of the graph node */
+class Successor 
+{
     public:
         Vector2 state;
         Action action;
 
     Successor(Vector2 s, Action a): state(s), action(a) {}
-} ;
+};
 
+/* 
+ * Represents a graph node.  
+ * Note: While we generate the random map, the maze is the graph and map cells are the graph nodes 
+ */
 class Node
 {
     private:
@@ -35,33 +41,37 @@ class Node
         Vector2 getState() const;
         const Node* getParent() const;
         const Action getAction() const;
-
 };
 
+/*
+ * Implementation of depth first search in graph 
+ */
 class DFS
 {
 private:
     Cell** m_maze;
 
-    int m_width;
-    int m_height;
-
-    bool m_end();
+    int m_width;    //> Maze or graph width
+    int m_height;   //> Maze or graph height
+    
+    /* Get the possible directions while being in the `pos` cell */
     std::vector<Action> m_validDirections(const Vector2& pos);
+    /* Returns all the childrens of a specific node */
     std::vector<Successor> m_getChildren(const Node& n);
 
 public:
     DFS(int height, int width);
 
-    Cell** generate();
+    Cell** generate();          //> Generate the random maze
+    void visit(const Node& n);  //> After DFS falls in a new node
+
+    
+    Vector2 getStartState() const; //> Get DFS initial state
+    
     int getHeight();
     int getWidth();
     Cell** getMaze();
 
     void destroy();
-
-    void visit(const Node& n);
-    Vector2 getStartState() const;
-    void m_visitThroug(const Node& n);
 };
 
